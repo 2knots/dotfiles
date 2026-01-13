19309052306394c03cab9dee5d67c27abc4d2f51
2knots <qazplmwsxoknedcijb6@gmail.com>
2026-01-13 18:17:39 +0900

    update alacritty_install.sh

--- alacritty_install.sh
+++ alacritty_install.sh
@@ -4,8 +4,7 @@ set -e
 
 WORK_DIR=$(cd $(dirname $0); pwd)
 APPDATA_DIR=$(cmd.exe /c "echo %APPDATA%" 2> /dev/null)
-APPDATA_DIR=${APPDATA_DIR//\\//}
-APPDATA_DIR=${APPDATA_DIR/C://mnt/c}
+APPDATA_DIR=$(wslpath "${APPDATA_DIR}" | tr -d '\r')
 mkdir -p ${APPDATA_DIR}/alacritty
 cp ${WORK_DIR}/alacritty.toml ${APPDATA_DIR}/alacritty/
 echo "alacritty setting completed!"
