1ce556a2da82fadf4d077e2dbfec21a0189d6552
2knots <qazplmwsxoknedcijb6@gmail.com>
2026-03-18 00:49:00 +0900

    Add new colorscheme for wsltty

--- wsltty_install.sh
+++ wsltty_install.sh
@@ -6,5 +6,5 @@ WORK_DIR=$(cd $(dirname $0); pwd)
 APPDATA_DIR=$(cmd.exe /c "echo %APPDATA%" 2> /dev/null)
 APPDATA_DIR=$(wslpath "${APPDATA_DIR}" | tr -d '\r')
 cp ${WORK_DIR}/minttyrc ${APPDATA_DIR}/wsltty/config
-cp ${WORK_DIR}/molokai-custom.minttyrc ${APPDATA_DIR}/wsltty/themes/
+cp ${WORK_DIR}/dracula_custom.minttyrc ${APPDATA_DIR}/wsltty/themes/
 echo "wsltty setting completed!"
